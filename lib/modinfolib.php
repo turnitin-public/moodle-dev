45e89b63b1b019609f057fd2be335caeadd2b14b
Jake Dallimore <jake@moodle.com>
2016-07-29 16:17:16 +0800

    MDL-54751 core: introduce deletion flag for modules and sections
    Created a flag, 'deletioninprogress' to be used when backgrounding the
    deletion of course modules and course sections. Modified modinfolib
    caches such that, when rebuilt, these caches hide the relevant modules
    and sections based on the value of the flag.

--- lib/modinfolib.php
+++ lib/modinfolib.php
@@ -554,8 +554,7 @@ class course_modinfo {
 
         // Get section data
         $sections = $DB->get_records('course_sections', array('course' => $course->id), 'section',
-                'section, id, course, name, summary, summaryformat, sequence, visible, ' .
-                'availability');
+                'section, id, course, name, summary, summaryformat, sequence, visible, availability, deletioninprogress');
         $compressedsections = array();
 
         $formatoptionsdef = course_get_format($course)->section_format_options();
@@ -753,6 +752,7 @@ class course_modinfo {
  * @property-read mixed $customdata Optional custom data stored in modinfo cache for this activity, or null if none
  * @property-read string $afterlink Extra HTML code to display after link - calculated on request
  * @property-read string $afterediticons Extra HTML code to display after editing icons (e.g. more icons) - calculated on request
+ * @property-read bool $deletioninprogress True if this course module is scheduled for deletion, false otherwise.
  */
 class cm_info implements IteratorAggregate {
     /**
@@ -1039,6 +1039,11 @@ class cm_info implements IteratorAggregate {
     private $afterediticons;
 
     /**
+     * @var bool representing the deletion state of the module. True if the mod is scheduled for deletion.
+     */
+    private $deletioninprogress;
+
+    /**
      * List of class read-only properties and their getter methods.
      * Used by magic functions __get(), __isset(), __empty()
      * @var array
@@ -1089,6 +1094,7 @@ class cm_info implements IteratorAggregate {
         'uservisible' => 'get_user_visible',
         'visible' => false,
         'visibleold' => false,
+        'deletioninprogress' => false
     );
 
     /**
@@ -1505,7 +1511,7 @@ class cm_info implements IteratorAggregate {
         static $cmfields = array('id', 'course', 'module', 'instance', 'section', 'idnumber', 'added',
             'score', 'indent', 'visible', 'visibleold', 'groupmode', 'groupingid',
             'completion', 'completiongradeitemnumber', 'completionview', 'completionexpected',
-            'showdescription', 'availability');
+            'showdescription', 'availability', 'deletioninprogress');
         foreach ($cmfields as $key) {
             $cmrecord->$key = $this->$key;
         }
@@ -1700,6 +1706,7 @@ class cm_info implements IteratorAggregate {
         $this->added = isset($mod->added) ? $mod->added : 0;
         $this->score = isset($mod->score) ? $mod->score : 0;
         $this->visibleold = isset($mod->visibleold) ? $mod->visibleold : 0;
+        $this->deletioninprogress = isset($mod->deletioninprogress) ? $mod->deletioninprogress : 0;
 
         // Note: it saves effort and database space to always include the
         // availability and completion fields, even if availability or completion
@@ -1860,6 +1867,12 @@ class cm_info implements IteratorAggregate {
             return null;
         }
         $this->uservisible = true;
+
+        // If the module is being deleted, set the uservisible state to false and return.
+        if ($this->deletioninprogress) {
+            $this->uservisible = false;
+            return null;
+        }
 
         // If the user cannot access the activity set the uservisible flag to false.
         // Additional checks are required to determine whether the activity is entirely hidden or just greyed out.
@@ -2454,6 +2467,11 @@ class section_info implements IteratorAggregate {
      */
     private $_uservisible;
 
+    /** True if the section has be scheduled for removal.
+     * @var bool
+     */
+    private $_deletioninprogress;
+
     /**
      * Default values for sectioncache fields; if a field has this value, it won't
      * be stored in the sectioncache cache, to save space. Checks are done by ===
@@ -2466,6 +2484,7 @@ class section_info implements IteratorAggregate {
         'summaryformat' => '1', // FORMAT_HTML, but must be a string
         'visible' => '1',
         'availability' => null,
+        'deletioninprogress' => null
     );
 
     /**
@@ -2609,6 +2628,9 @@ class section_info implements IteratorAggregate {
      */
     private function get_available() {
         global $CFG;
+        if ($this->_deletioninprogress) {
+            return false;
+        }
         $userid = $this->modinfo->get_user_id();
         if ($this->_available !== null || $userid == -1) {
             // Has already been calculated or does not need calculation.
@@ -2675,6 +2697,9 @@ class section_info implements IteratorAggregate {
      * @return bool
      */
     private function get_uservisible() {
+        if ($this->_deletioninprogress) {
+            return false;
+        }
         $userid = $this->modinfo->get_user_id();
         if ($this->_uservisible !== null || $userid == -1) {
             // Has already been calculated or does not need calculation.

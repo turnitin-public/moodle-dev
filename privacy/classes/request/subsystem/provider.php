452498da16998269ad7fd958adcf8c9c8541f4a7
Jake Dallimore <jake@moodle.com>
2018-02-14 11:34:30 +0800

    MDL-61307 core_privacy: fix core provider namespace

--- privacy/classes/request/subsystem/provider.php
+++ privacy/classes/request/subsystem/provider.php
@@ -25,7 +25,7 @@
  *
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-namespace core_privacy\request\plugin;
+namespace core_privacy\request\subsystem;
 
 /**
  * The provider interface for plugins which provide data from a subsystem

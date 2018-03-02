abgreeve/moodle

MDL-61307 core_privacy: Define and test providers

privacy/classes/metadata/provider.php
@@ -1,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the core_privacy\metadata_provider interface.
+ *
+ * @package core_privacy
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core_privacy\metadata;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Interface component_metadata_provider
+ *
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+interface provider {
+
+    /**
+     * Returns meta data about this system.
+     *
+     * @param   item_collection     $itemcollection The initialised item collection to add items to.
+     * @return  item_collection     A listing of user data stored through this system.
+     */
+    public static function get_metadata(item_collection $itemcollection) : item_collection ;
+}

privacy/classes/metadata/item_record/database_table.php
@@ -1,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines an item of metadata which encapsulates a database table.
+ *
+ * @package core_privacy
+ * @copyright 2018 Zig Tan <zig@moodle.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core_privacy\metadata\item_record;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The database_table item record.
+ *
+ * @copyright 2018 Zig Tan <zig@moodle.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class database_table implements type {
+
+    /**
+     * @var string  Database table name.
+     */
+    protected $name;
+
+    /**
+     * @var array Fields which contain user information within the table.
+     */
+    protected $privacyfields;
+
+    /**
+     * @var string  A description of what this table is used for.
+     */
+    protected $summary;
+
+    /**
+     * Constructor to create a new database_table item_record.
+     *
+     * @param   string  $name The name of the database table being described.
+     * @param   array   $privacyfields A list of fields iwth their description.
+     * @param   string  $summary A description of what the table is used for.
+     */
+    public function __construct($name, array $privacyfields = [], $summary = '') {
+        if (debugging('', DEBUG_DEVELOPER)) {
+            if (empty($privacyfields)) {
+                debugging("Table '{$name}' was supplied without any fields.", DEBUG_DEVELOPER);
+            }
+
+            foreach ($privacyfields as $key => $field) {
+                $teststring = clean_param($field, PARAM_STRINGID);
+                if ($teststring !== $field) {
+                    debugging("Field '{$key}' passed for table '{$name}' has an invalid langstring identifier: '{$field}'",
+                        DEBUG_DEVELOPER);
+                }
+            }
+
+            $teststring = clean_param($summary, PARAM_STRINGID);
+            if ($teststring !== $summary) {
+                debugging("Summary information for the '{$name}' table has an invalid langstring identifier: '{$summary}'",
+                    DEBUG_DEVELOPER);
+            }
+        }
+
+        $this->name = $name;
+        $this->privacyfields = $privacyfields;
+        $this->summary = $summary;
+    }
+
+    /**
+     * The name of the database table.
+     *
+     * @return  string
+     */
+    public function get_name() {
+        return $this->name;
+    }
+
+    /**
+     * The list of fields within the table which contain user data, with a description of each field.
+     *
+     * @return  array
+     */
+    public function get_privacy_fields() {
+        return $this->privacyfields;
+    }
+
+    /**
+     * A summary of what this table is used for.
+     *
+     * @return  string
+     */
+    public function get_summary() {
+        return $this->summary;
+    }
+}

privacy/classes/metadata/item_record/user_preference.php
@@ -1,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * An item record which encapsulates a set of user preferences.
+ *
+ * @package    core_privacy
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_privacy\metadata\item_record;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The user_preference item record.
+ *
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class user_preference implements type {
+
+    /**
+     * @var The name of this user prefence.
+     */
+    protected $name;
+
+    /**
+     * @var A description of what this user preference means.
+     */
+    protected $summary;
+
+    /**
+     * Constructor to create a new user_preference item_record.
+     *
+     * @param   string  $name The name of the user preference.
+     * @param   string  $summary A description of what the preference is used for.
+     */
+    public function __construct($name, $summary = '') {
+        if (debugging('', DEBUG_DEVELOPER)) {
+            $teststring = clean_param($summary, PARAM_STRINGID);
+            if ($teststring !== $summary) {
+                debugging("Summary information for use of the '{$name}' subsystem " .
+                    " has an invalid langstring identifier: '{$summary}'",
+                    DEBUG_DEVELOPER);
+            }
+        }
+
+        $this->name = $name;
+        $this->summary = $summary;
+    }
+
+    /**
+     * The name of the user preference.
+     *
+     * @return  string
+     */
+    public function get_name() {
+        return $this->name;
+    }
+
+    /**
+     * A user preference encapsulates a single field and has no sub-fields.
+     *
+     * @return  array
+     */
+    public function get_privacy_fields() {
+        return null;
+    }
+
+    /**
+     * A summary of what this user preference is used for.
+     *
+     * @return  string
+     */
+    public function get_summary() {
+        return $this->summary;
+    }
+}

privacy/classes/request/writer.php
@@ -1,0 +1,118 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the interface required to implmeent a content writer.
+ *
+ * @package core_privacy
+ * @copyright 2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core_privacy\request;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The writer factory class used to fetch and work with the content_writer.
+ *
+ * @copyright 2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class writer {
+    /**
+     * @var writer The singleton instance of this writer.
+     */
+    protected static $instance = null;
+
+    /**
+     * @var content_writer The current content_writer instance.
+     */
+    protected $realwriter = null;
+
+    /**
+     * Constructor for the content writer.
+     *
+     * Protected to prevent direct instantiation.
+     */
+    protected function __construct() {
+    }
+
+    /**
+     * Singleton to return or create and return a copy of a content_writer.
+     *
+     * @return  content_writer
+     */
+    protected function get_writer_instance() : content_writer {
+        if (null === $this->realwriter) {
+            if (PHPUNIT_TEST) {
+                $this->realwriter = new \core_privacy\phpunit\request\content_writer(static::instance());
+            } else {
+                $this->realwriter = new moodle_content_writer(static::instance());
+            }
+        }
+
+        return $this->realwriter;
+    }
+
+    /**
+     * Return an instance of
+     */
+    protected static final function instance() {
+        if (null === self::$instance) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Reset the writer and content_writer.
+     */
+    public static final function reset() {
+        static::$instance = null;
+    }
+
+    /**
+     * Provide an instance of the writer with the specified context applied.
+     *
+     * @param   \context        $context    The context to apply
+     * @return  content_writer              The content_writer
+     */
+    public static function with_context(\context $context) : content_writer {
+        return static::instance()
+            ->get_writer_instance()
+            ->set_context($context);
+    }
+
+    /**
+     * Export the specified user preference.
+     *
+     * @param   string          $component  The name of the component.
+     * @param   string          $key        The name of th key to be exported.
+     * @param   string          $value      The value of the preference
+     * @param   string          $description    A description of the value
+     * @return  content_writer
+     */
+    public static function export_user_preference(
+        string $component,
+        string $key,
+        string $value,
+        string $description
+    ) : content_writer {
+        return static::with_context(\context_system::instance())
+            ->export_user_preference($component, $key, $value, $description);
+    }
+}

turnitin-public/moodle-dev

privacy/classes/metadata/provider.php
@@ -1,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the core_privacy\metadata_provider interface.
+ *
+ * @package core_privacy
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core_privacy\metadata;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Interface component_metadata_provider
+ *
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+interface provider {
+
+    /**
+     * Returns meta data about this system.
+     *
+     * @param   collection     $collection The initialised collection to add items to.
+     * @return  collection     A listing of user data stored through this system.
+     */
+    public static function get_metadata(collection $collection) : collection ;
+}

privacy/classes/metadata/types/database_table.php
@@ -1,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines an item of metadata which encapsulates a database table.
+ *
+ * @package core_privacy
+ * @copyright 2018 Zig Tan <zig@moodle.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core_privacy\metadata\types;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The database_table type.
+ *
+ * @copyright 2018 Zig Tan <zig@moodle.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class database_table implements type {
+
+    /**
+     * @var string  Database table name.
+     */
+    protected $name;
+
+    /**
+     * @var array Fields which contain user information within the table.
+     */
+    protected $privacyfields;
+
+    /**
+     * @var string  A description of what this table is used for.
+     */
+    protected $summary;
+
+    /**
+     * Constructor to create a new database_table type.
+     *
+     * @param   string  $name The name of the database table being described.
+     * @param   array   $privacyfields A list of fields with their description.
+     * @param   string  $summary A description of what the table is used for.
+     */
+    public function __construct($name, array $privacyfields = [], $summary = '') {
+        if (debugging('', DEBUG_DEVELOPER)) {
+            if (empty($privacyfields)) {
+                debugging("Table '{$name}' was supplied without any fields.", DEBUG_DEVELOPER);
+            }
+
+            foreach ($privacyfields as $key => $field) {
+                $teststring = clean_param($field, PARAM_STRINGID);
+                if ($teststring !== $field) {
+                    debugging("Field '{$key}' passed for table '{$name}' has an invalid langstring identifier: '{$field}'",
+                        DEBUG_DEVELOPER);
+                }
+            }
+
+            $teststring = clean_param($summary, PARAM_STRINGID);
+            if ($teststring !== $summary) {
+                debugging("Summary information for the '{$name}' table has an invalid langstring identifier: '{$summary}'",
+                    DEBUG_DEVELOPER);
+            }
+        }
+
+        $this->name = $name;
+        $this->privacyfields = $privacyfields;
+        $this->summary = $summary;
+    }
+
+    /**
+     * The name of the database table.
+     *
+     * @return  string
+     */
+    public function get_name() {
+        return $this->name;
+    }
+
+    /**
+     * The list of fields within the table which contain user data, with a description of each field.
+     *
+     * @return  array
+     */
+    public function get_privacy_fields() {
+        return $this->privacyfields;
+    }
+
+    /**
+     * A summary of what this table is used for.
+     *
+     * @return  string
+     */
+    public function get_summary() {
+        return $this->summary;
+    }
+}

privacy/classes/metadata/types/user_preference.php
@@ -1,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines an item of metadata which encapsulates a user's preferences.
+ *
+ * @package    core_privacy
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_privacy\metadata\types;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The user_preference type.
+ *
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class user_preference implements type {
+
+    /**
+     * @var The name of this user preference.
+     */
+    protected $name;
+
+    /**
+     * @var A description of what this user preference means.
+     */
+    protected $summary;
+
+    /**
+     * Constructor to create a new user_preference types.
+     *
+     * @param   string  $name The name of the user preference.
+     * @param   string  $summary A description of what the preference is used for.
+     */
+    public function __construct($name, $summary = '') {
+        if (debugging('', DEBUG_DEVELOPER)) {
+            $teststring = clean_param($summary, PARAM_STRINGID);
+            if ($teststring !== $summary) {
+                debugging("Summary information for use of the '{$name}' subsystem " .
+                    " has an invalid langstring identifier: '{$summary}'",
+                    DEBUG_DEVELOPER);
+            }
+        }
+
+        $this->name = $name;
+        $this->summary = $summary;
+    }
+
+    /**
+     * The name of the user preference.
+     *
+     * @return  string
+     */
+    public function get_name() {
+        return $this->name;
+    }
+
+    /**
+     * A user preference encapsulates a single field and has no sub-fields.
+     *
+     * @return  array
+     */
+    public function get_privacy_fields() {
+        return null;
+    }
+
+    /**
+     * A summary of what this user preference is used for.
+     *
+     * @return  string
+     */
+    public function get_summary() {
+        return $this->summary;
+    }
+}

privacy/classes/request/writer.php
@@ -1,0 +1,118 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the interface required to implmeent a content writer.
+ *
+ * @package core_privacy
+ * @copyright 2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core_privacy\request;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The writer factory class used to fetch and work with the content_writer.
+ *
+ * @copyright 2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class writer {
+    /**
+     * @var writer The singleton instance of this writer.
+     */
+    protected static $instance = null;
+
+    /**
+     * @var content_writer The current content_writer instance.
+     */
+    protected $realwriter = null;
+
+    /**
+     * Constructor for the content writer.
+     *
+     * Protected to prevent direct instantiation.
+     */
+    protected function __construct() {
+    }
+
+    /**
+     * Singleton to return or create and return a copy of a content_writer.
+     *
+     * @return  content_writer
+     */
+    protected function get_writer_instance() : content_writer {
+        if (null === $this->realwriter) {
+            if (PHPUNIT_TEST) {
+                $this->realwriter = new \core_privacy\tests\request\content_writer(static::instance());
+            } else {
+                $this->realwriter = new moodle_content_writer(static::instance());
+            }
+        }
+
+        return $this->realwriter;
+    }
+
+    /**
+     * Return an instance of
+     */
+    protected static final function instance() {
+        if (null === self::$instance) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Reset the writer and content_writer.
+     */
+    public static final function reset() {
+        static::$instance = null;
+    }
+
+    /**
+     * Provide an instance of the writer with the specified context applied.
+     *
+     * @param   \context        $context    The context to apply
+     * @return  content_writer              The content_writer
+     */
+    public static function with_context(\context $context) : content_writer {
+        return static::instance()
+            ->get_writer_instance()
+            ->set_context($context);
+    }
+
+    /**
+     * Export the specified user preference.
+     *
+     * @param   string          $component  The name of the component.
+     * @param   string          $key        The name of th key to be exported.
+     * @param   string          $value      The value of the preference
+     * @param   string          $description    A description of the value
+     * @return  content_writer
+     */
+    public static function export_user_preference(
+        string $component,
+        string $key,
+        string $value,
+        string $description
+    ) : content_writer {
+        return static::with_context(\context_system::instance())
+            ->export_user_preference($component, $key, $value, $description);
+    }
+}
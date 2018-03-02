abgreeve/moodle

MDL-61307 core_privacy: Add the privacy manager

privacy/tests/fixtures/mock_plugin_subplugin_provider.php
@@ -1,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Test provider using a fake plugin name.
+ *
+ * @package core_privacy
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_testcomponent3\privacy;
+
+use core_privacy\metadata\item_collection;
+
+/**
+ * Mock shared_data_provider for unit tests.
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\metadata\provider, \core_privacy\request\plugin\subplugin_provider {
+    /**
+     * @return array The array of metadata.
+     */
+    public static function get_metadata(\core_privacy\metadata\item_collection $itemcollection)
+        : \core_privacy\metadata\item_collection {
+        $itemcollection = new item_collection('testcomponent3');
+        $itemcollection->add_database_table('testtable', ['testfield1', 'testfield2'], 'testsummary');
+        return $itemcollection;
+    }
+}

privacy/tests/fixtures/mock_provider.php
@@ -1,0 +1,84 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Test provider using a fake plugin name.
+ *
+ * @package core_privacy
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_testcomponent\privacy;
+
+use core_privacy\metadata\item_collection;
+use core_privacy\request\approved_contextlist;
+use core_privacy\request\contextlist;
+use core_privacy\request\deletion_criteria;
+
+/**
+ * Mock core_user_data_provider for unit tests.
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\metadata\provider, \core_privacy\request\plugin\provider {
+    /**
+     * @return array The array of metadata.
+     */
+    public static function get_metadata(\core_privacy\metadata\item_collection $itemcollection)
+        : \core_privacy\metadata\item_collection {
+        $itemcollection = new item_collection('testcomponent');
+        $itemcollection->add_database_table('testtable', ['testfield1', 'testfield2'], 'testsummary');
+        return $itemcollection;
+    }
+
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param   int $userid The user to search.
+     * @return  contextlist   $contextlist  The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist {
+        return new contextlist();
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts.
+     *
+     * @param   approved_contextlist $contextlist The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist) {
+        // This does nothing. We only want to confirm this can be called via the \core_privacy\manager.
+    }
+
+    /**
+     * Delete all use data which matches the specified deletion_criteria.
+     *
+     * @param   deletion_criteria $criteria An object containing specific deletion criteria to delete for.
+     */
+    public static function delete_for_context(deletion_criteria $criteria) {
+        // This does nothing. We only want to confirm this can be called via the \core_privacy\manager.
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param   approved_contextlist $contextlist The approved contexts and user information to delete information for.
+     */
+    public static function delete_user_data(approved_contextlist $contextlist) {
+        // This does nothing. We only want to confirm this can be called via the \core_privacy\manager.
+    }
+}

turnitin-public/moodle-dev

privacy/tests/fixtures/mock_plugin_subplugin_provider.php
@@ -1,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Test provider using a fake plugin name.
+ *
+ * @package core_privacy
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_testcomponent3\privacy;
+
+use core_privacy\metadata\collection;
+
+/**
+ * Mock shared_data_provider for unit tests.
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\metadata\provider, \core_privacy\request\plugin\subplugin_provider {
+    /**
+     * @return array The array of metadata.
+     */
+    public static function get_metadata(\core_privacy\metadata\collection $collection)
+        : \core_privacy\metadata\collection {
+        $collection = new collection('testcomponent3');
+        $collection->add_database_table('testtable', ['testfield1', 'testfield2'], 'testsummary');
+        return $collection;
+    }
+}

privacy/tests/fixtures/mock_provider.php
@@ -1,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Test provider using a fake plugin name.
+ *
+ * @package core_privacy
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_testcomponent\privacy;
+
+use core_privacy\metadata\collection;
+use core_privacy\request\approved_contextlist;
+use core_privacy\request\contextlist;
+use core_privacy\request\deletion_criteria;
+
+/**
+ * Mock core_user_data_provider for unit tests.
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\metadata\provider, \core_privacy\request\plugin\provider {
+    /**
+     * @return array The array of metadata.
+     */
+    public static function get_metadata(\core_privacy\metadata\collection $collection)
+        : \core_privacy\metadata\collection {
+        $collection = new collection('testcomponent');
+        $collection->add_database_table('testtable', ['testfield1', 'testfield2'], 'testsummary');
+        return $collection;
+    }
+
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param   int $userid The user to search.
+     * @return  contextlist   $contextlist  The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist {
+        $cl = new contextlist();
+        $cl->add_from_sql("SELECT c.id FROM {context} c WHERE c.id = :id", ['id' => \context_system::instance()->id]);
+
+        return $cl;
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts.
+     *
+     * @param   approved_contextlist $contextlist The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist) {
+        // This does nothing. We only want to confirm this can be called via the \core_privacy\manager.
+    }
+
+    /**
+     * Delete all use data which matches the specified deletion_criteria.
+     *
+     * @param   deletion_criteria $criteria An object containing specific deletion criteria to delete for.
+     */
+    public static function delete_for_context(deletion_criteria $criteria) {
+        // This does nothing. We only want to confirm this can be called via the \core_privacy\manager.
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param   approved_contextlist $contextlist The approved contexts and user information to delete information for.
+     */
+    public static function delete_user_data(approved_contextlist $contextlist) {
+        // This does nothing. We only want to confirm this can be called via the \core_privacy\manager.
+    }
+}
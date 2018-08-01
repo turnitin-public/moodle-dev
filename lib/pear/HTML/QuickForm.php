aae150841b83f26003b7c6891f35c11f903178b9
Jake Dallimore <jake@moodle.com>
2018-08-01 13:31:58 +0800

    MDL-63070 core_form: reapply the fix from 52826 to QuickForm.php
    Moves the client-side validation out of the global namespace.

--- lib/pear/HTML/QuickForm.php
+++ lib/pear/HTML/QuickForm.php
@@ -1112,9 +1112,6 @@ class HTML_QuickForm extends HTML_Common
         if (!isset($this->_rules[$element])) {
             $this->_rules[$element] = array();
         }
-        if ($validation == 'client') {
-            $this->updateAttributes(array('onsubmit' => 'try { var myValidator = validate_' . $this->_attributes['id'] . '; } catch(e) { return true; } return myValidator(this);'));
-        }
         $this->_rules[$element][] = array(
             'type'        => $type,
             'format'      => $format,
@@ -1183,9 +1180,6 @@ class HTML_QuickForm extends HTML_Common
                         $this->_required[] = $elementName;
                         $required++;
                     }
-                    if ('client' == $validation) {
-                        $this->updateAttributes(array('onsubmit' => 'try { var myValidator = validate_' . $this->_attributes['id'] . '; } catch(e) { return true; } return myValidator(this);'));
-                    }
                 }
             }
             if ($required > 0 && count($groupObj->getElements()) == $required) {
@@ -1216,9 +1210,6 @@ class HTML_QuickForm extends HTML_Common
                                             'reset'      => $reset);
             if ($type == 'required') {
                 $this->_required[] = $group;
-            }
-            if ($validation == 'client') {
-                $this->updateAttributes(array('onsubmit' => 'try { var myValidator = validate_' . $this->_attributes['id'] . '; } catch(e) { return true; } return myValidator(this);'));
             }
         }
     } // end func addGroupRule

96ddfa54ce751ad36200fbf1d1e141e5e9ad4a7e
Jake Dallimore <jake@moodle.com>
2018-08-03 16:13:51 +0800

    MDL-63070 core_form: reapply anon function fix from 60281 to QuickForms

--- lib/pear/HTML/QuickForm/Rule/Compare.php
+++ lib/pear/HTML/QuickForm/Rule/Compare.php
@@ -81,13 +81,16 @@ class HTML_QuickForm_Rule_Compare extends HTML_QuickForm_Rule
     function validate($values, $operator = null)
     {
         $operator = $this->_findOperator($operator);
-        if ('===' != $operator && '!==' != $operator) {
-            $compareFn = create_function('$a, $b', 'return floatval($a) ' . $operator . ' floatval($b);');
+        $a = $values[0];
+        $b = $values[1];
+        if ($operator == '==') {
+            return $a == $b;
+        } else if ($operator == '!=') {
+            return $a != $b;
         } else {
-            $compareFn = create_function('$a, $b', 'return strval($a) ' . $operator . ' strval($b);');
+            // One of: <= , >= , < , > .
+            return eval('return ' . floatval($a) . $operator . floatval($b) . ';');
         }
-        
-        return $compareFn($values[0], $values[1]);
     }
 
 

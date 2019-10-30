4fc82cf66d87d93db812c76d5c35f56b38009472
Bas Brands <bas@moodle.com>
2019-10-30 10:32:21 +0100

    MDL-66868 atto_h5p: add allow download option for mobile

--- lib/editor/atto/plugins/h5p/lang/en/atto_h5p.php
+++ lib/editor/atto/plugins/h5p/lang/en/atto_h5p.php
@@ -22,6 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['allowoffline'] = 'Allow H5P file to be downloaded for mobile app offline use';
 $string['browserepositories'] = 'Browse repositories...';
 $string['copyrightbutton'] = 'Copyright button';
 $string['downloadbutton'] = 'Allow download';

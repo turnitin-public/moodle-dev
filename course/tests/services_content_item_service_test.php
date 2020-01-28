298d93dd49a03a44ae3c5455b02a22420ddcd44e
Jake Dallimore <jake@moodle.com>
2020-01-28 16:47:09 +0800

    MDL-67585 core_course: only accept content items from mods and submods
    This matches the prior behaviour in core.

--- course/tests/services_content_item_service_test.php
+++ course/tests/services_content_item_service_test.php
@@ -103,4 +103,38 @@ class services_content_item_service_testcase extends \advanced_testcase {
             $this->assertStringContainsString('sr=7', $item->link);
         }
     }
+
+    /**
+     * Test that the service can produce same display data as the legacy function, get_module_metadata().
+     */
+    public function test_get_content_item_for_user_in_course_legacy_compatibility() {
+        $this->resetAfterTest();
+        global $COURSE;
+
+        // Create a user in a course.
+        $course = $this->getDataGenerator()->create_course();
+        $user = $this->getDataGenerator()->create_and_enrol($course, 'editingteacher');
+
+        $cis = new content_item_service(new content_item_readonly_repository());
+        $contentitems = $cis->get_content_items_for_user_in_course($user, $course, ['sr' => 7]);
+
+        $this->setUser($user);
+        $COURSE = $course; // The get_shortcuts hook implementations use the global course, so it must be set.
+        $legacydata = get_module_metadata($course, get_module_types_names(), 7);
+
+        $this->assertEquals(count($legacydata), count($contentitems));
+
+        foreach ($legacydata as $legacyitem) {
+            $contentitem = array_shift($contentitems);
+            // The 'name' property had the link concatenated in some cases in the legacy data,
+            // depending on whether the callback provided just the core module item or additional items.
+            // The new service does not augment names like this, so we cannot compare the 2.
+            $this->assertEquals($legacyitem->title, $contentitem->title);
+            $this->assertEquals(explode('?', $legacyitem->link->out(false))[0], explode('?', $contentitem->link)[0]);
+            $this->assertEquals($legacyitem->icon, $contentitem->icon);
+            $this->assertEquals($legacyitem->help, $contentitem->help);
+            $this->assertEquals($legacyitem->archetype, $contentitem->archetype);
+        }
+        $this->assertDebuggingCalled();
+    }
 }

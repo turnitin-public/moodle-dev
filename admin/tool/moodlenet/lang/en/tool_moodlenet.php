6aa047ca10a001ef0175c103f365c6879246e8de
Jake Dallimore <jake@moodle.com>
2020-03-04 15:36:14 +0800

    MDL-67887 tool_moodlenet: Add plugin shell with import handler
    Just contains a bare bones tool, with:
    - An import endpoint which can be posted to from MoodleNet
    - index.php which just reports the data posted from MoodleNet

---
+++ admin/tool/moodlenet/lang/en/tool_moodlenet.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the tool_moodlenet component.
+ *
+ * @package     tool_moodlenet
+ * @category    string
+ * @copyright   2020 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['enablemoodlenet'] = 'Enable the MoodleNet integration';
+$string['enablemoodlenetinfo'] = 'Enabling the integration allows users with the \'xx\' capability to browse MoodleNet from the
+activity chooser and import MoodleNet resources into their course. It also allows users to push backups from MoodleNet into Moodle.
+';
+$string['missinginvalidpostdata'] = 'The resource information from MoodleNet is either missing, or is in an incorrect format. 
+If this happens repeatedly, please contact the site administrator.';
+$string['moodlenetnotenabled'] = 'The MoodleNet integration must be enabled before resource imports can be processed.
+To enable this feature, see the \'enablemoodlenet\' setting.';
+$string['pluginname'] = 'MoodleNet';

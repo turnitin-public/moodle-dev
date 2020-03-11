a0135b83976318d21738fa855a08ecc6b154b3db
Mathew May <mathewm@hotmail.co.nz>
2020-03-11 16:40:06 +0800

    MDL-68141 tool_moodlenet: Add course selection page

--- admin/tool/moodlenet/lang/en/tool_moodlenet.php
+++ admin/tool/moodlenet/lang/en/tool_moodlenet.php
@@ -26,6 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['addingaresource'] = 'Adding a resource from MoodleNet';
+$string['clearsearch'] = "Clear search";
 $string['enablemoodlenet'] = 'Enable the MoodleNet integration';
 $string['enablemoodlenetinfo'] = 'Enabling the integration allows users with the \'xx\' capability to browse MoodleNet from the
 activity chooser and import MoodleNet resources into their course. It also allows users to push backups from MoodleNet into Moodle.
@@ -41,6 +42,14 @@ $string['mnetprofile'] = 'MoodleNet profile';
 $string['mnetprofiledesc'] = '<p>Enter in your MoodleNet profile details here to be redirected to your profile while visiting MoodleNet.</p>';
 $string['moodlenetnotenabled'] = 'The MoodleNet integration must be enabled before resource imports can be processed.
 To enable this feature, see the \'enablemoodlenet\' setting.';
+$string['notification'] = 'You are currently adding the resource "{$a->name}". Navigate to the course you want to add the content to, or <a href="{$a->cancellink}">Cancel</a>.';
+$string['searchcourses'] = "Search courses";
+$string['selectacourseinfo'] = 'Please select the course in which the resource "{$a}" will be added';
+$string['selecthelp'] = 'We have narrowed down the list of courses where you have the permission to create resources. If you can not find a course, please contact your Moodle administrator.';
+$string['selectpagetitle'] = 'Select page';
 $string['pluginname'] = 'MoodleNet';
 $string['privacy:metadata'] = "The MoodleNet tool only facilitates communication with MoodleNet. It stores no data.";
+$string['profilevalidationerror'] = 'There was a problem trying to validate your profile URL';
+$string['profilevalidationfail'] = 'Please enter a valid MoodleNet profile URL';
+$string['profilevalidationpass'] = 'Looks good!';
 $string['uploadlimitexceeded'] = 'The file size {$a->filesize} exceeds the user upload limit of {$a->uploadlimit} bytes.';

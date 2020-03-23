14da9b55b4f2d982bb030c49e52e319d65b0c266
Adrian Greeve <abgreeve@gmail.com>
2020-03-23 16:09:39 +0800

    MDL-67885 lib: Add moodlenet user profile information.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020050600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2020050600.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '3.9dev+ (Build: 20200506)'; // Human-friendly version name

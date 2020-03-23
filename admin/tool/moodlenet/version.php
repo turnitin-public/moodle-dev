97d76d1260af2f16782aa1675d10a8b5539fe270
Adrian Greeve <abgreeve@gmail.com>
2020-03-23 16:09:14 +0800

    MDL-67885 tool_moodlenet: Added a moodlenet profile field.

--- admin/tool/moodlenet/version.php
+++ admin/tool/moodlenet/version.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Version file for tool_moodlenet
+ *
  * @package     tool_moodlenet
  * @copyright   2020 Jake Dallimore <jrhdallimore@gmail.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -23,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'tool_moodlenet';
-$plugin->version    = 2020030500;
+$plugin->version    = 2020031100;
 $plugin->requires   = 2020022800.01;
 $plugin->maturity   = MATURITY_ALPHA;

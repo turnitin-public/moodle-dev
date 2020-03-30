53b51e6882b6517cf0ad213edebdb7a9cabb6d40
Adrian Greeve <abgreeve@gmail.com>
2020-03-30 14:36:15 +0800

    MDL-68142 tool_moodlenet: Move setting out of core.
    Currently the enablemoodlenet setting is sitting in core. This
    commit moves it into the plugin where it should be.

--- admin/tool/moodlenet/instance.php
+++ admin/tool/moodlenet/instance.php
@@ -27,7 +27,7 @@ require_once(__DIR__ . '/../../../config.php');
 require_login();
 
 // The integration must be enabled for this import endpoint to be active.
-if (!get_config('core', 'enablemoodlenet')) {
+if (!get_config('tool_moodlenet', 'enablemoodlenet')) {
     print_error('moodlenetnotenabled', 'tool_moodlenet');
 }
 

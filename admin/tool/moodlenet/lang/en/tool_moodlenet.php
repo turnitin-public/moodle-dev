2a76a0baeab3215c68f4c04290323eafa6e797e4
Jake Dallimore <jake@moodle.com>
2020-04-15 11:56:06 +0800

    MDL-67889 tool_moodlenet: add import endpoint and confirmation page
    The endpoint serves to take a POST request and convert it to a GET for
    the secured confirmation page, letting us leverage the $wantsurl feature
    of auth.

--- admin/tool/moodlenet/lang/en/tool_moodlenet.php
+++ admin/tool/moodlenet/lang/en/tool_moodlenet.php
@@ -30,6 +30,8 @@ $string['enablemoodlenetinfo'] = 'Enabling the integration allows users with the
 activity chooser and import MoodleNet resources into their course. It also allows users to push backups from MoodleNet into Moodle.
 ';
 $string['errorduringdownload'] = 'An error occurred while downloading the file: {$a}';
+$string['importconfirm'] = 'You are about to add the resource "{$a->resourcename}" to the course "{$a->coursename}". Please confirm if this is what you intend to do.';
+$string['importconfirmnocourse'] = 'You are about to add the resource "{$a->resourcename}". Please confirm if this is what you intend to do.';
 $string['missinginvalidpostdata'] = 'The resource information from MoodleNet is either missing, or is in an incorrect format.
 If this happens repeatedly, please contact the site administrator.';
 $string['mnetprofile'] = 'MoodleNet profile';

afda21728ab0e5bf0a5a471f8fddfbb9c6c12f75
Jake Dallimore <jake@moodle.com>
2020-04-15 11:56:06 +0800

    MDL-67889 tool_moodlenet: add import endpoint and confirmation page
    The endpoint serves to take a POST request and convert it to a GET for
    the secured confirmation page, letting us leverage the $wantsurl feature
    of auth.

--- admin/tool/moodlenet/import.php
+++ admin/tool/moodlenet/import.php
@@ -1,4 +1,32 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This is the main endpoint which MoodleNet instances POST to.
+ *
+ * This converts the POST into a GET for a secured page, so the $wantsurl aspect of auth can be leveraged.
+ *
+ * Once the POST is converted to a GET request for admin/tool/moodlenet/index.php with relevant params, the user will either:
+ * - If not authenticated, they will be asked to login, after which they will see the confirmation page.
+ * - If authenticated, they will see the confirmation page.
+ *
+ * @package     tool_moodlenet
+ * @copyright   2020 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once(__DIR__ . '/../../../config.php');
 

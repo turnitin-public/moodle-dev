36cc2d4b5d42f3f0f558eb49dcbf28f808446d55
Jake Dallimore <jake@moodle.com>
2020-04-15 11:56:06 +0800

    MDL-67889 tool_moodlenet: add import endpoint and confirmation page
    The endpoint serves to take a POST request and convert it to a GET for
    the secured confirmation page, letting us leverage the $wantsurl feature
    of auth.

--- admin/tool/moodlenet/index.php
+++ admin/tool/moodlenet/index.php
@@ -15,12 +15,17 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Index page.
+ * Landing page for all imports from MoodleNet.
+ *
+ * This page asks the user to confirm the import process, and takes them to the relevant next step.
  *
  * @package     tool_moodlenet
  * @copyright   2020 Jake Dallimore <jrhdallimore@gmail.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+use tool_moodlenet\local\remote_resource;
+use tool_moodlenet\local\url;
 
 require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->dirroot .'/course/lib.php');
@@ -29,22 +34,68 @@ $resourceurl = required_param('resourceurl', PARAM_RAW);
 $resourceurl = urldecode($resourceurl);
 $course = optional_param('course', null, PARAM_INT);
 $section = optional_param('section', null, PARAM_INT);
+$cancel = optional_param('cancel', null, PARAM_TEXT);
+$continue = optional_param('continue', null, PARAM_TEXT);
 
-// TODO: Might be a course context if we have that param in the URL, so check/set these two accordingly.
-require_login();
+require_login($course, false);
+if ($course) {
+    require_capability('moodle/course:manageactivities', context_course::instance($course));
+}
+
+// Handle the form submits.
+// This page POSTs to self to verify the sesskey for the confirm action.
+// The next page will either be:
+// - 1. The restore process for a course or module, if the file is an mbz file.
+// - 2. The 'select a course' tool page, if course and section are not provided.
+// - 3. The 'select what to do with the content' tool page, provided course and section are present.
+// - 4. The dashboard, if the user decides to cancel and course or section is not found.
+// - 5. The course home, if the user decides to cancel but the course and section are found.
+if ($cancel) {
+    $url = !empty($course) ? new \moodle_url('/course/view.php', ['id' => $course]) : new \moodle_url('/');
+    redirect($url);
+} else if ($continue) {
+    confirm_sesskey();
+
+    // Course and section data present and confirmed. Redirect to the option select view.
+    if (!is_null($course) && !is_null($section)) {
+        redirect(new \moodle_url('/admin/tool/moodlenet/options.php', [
+            'resourceurl' => urlencode($resourceurl),
+            'course' => $course,
+            'section' => $section,
+        ]));
+    }
+    // TODO: Extend conditional to handle cases where course needs to be selected or when the file is an mbz.
+}
+
+
+$remoteresource = new remote_resource(new curl(), new url($resourceurl));
+$extension = $remoteresource->get_extension();
+
+// Display the page.
 $PAGE->set_context(context_system::instance());
-
-// Page setup.
-$PAGE->set_pagelayout('standard');
-$PAGE->set_title("Some import handler page thingy");
-$PAGE->set_heading("Dummy index page");
+$PAGE->set_pagelayout('base');
+$PAGE->set_title(get_string('addingaresource', 'tool_moodlenet'));
+$PAGE->set_heading(get_string('addingaresource', 'tool_moodlenet'));
 $url = new moodle_url('/admin/tool/moodlenet/index.php');
 $PAGE->set_url($url);
+$renderer = $PAGE->get_renderer('core');
 
-// Dummy page.
 echo $OUTPUT->header();
-\core\notification::info("
-Resource: " . $resourceurl . "
-<br> Course: $course
-<br> Section $section");
+
+// Relevant confirmation form.
+$context = $context = [
+    'resourceurl' => $resourceurl,
+    'resourcename' => $remoteresource->get_name() . '.' . $remoteresource->get_extension(),
+    'sesskey' => sesskey()
+];
+if (!is_null($course) && !is_null($section)) {
+    $course = get_course($course);
+    $context = array_merge($context, [
+        'course' => $course->id,
+        'coursename' => $course->shortname,
+        'section' => $section
+    ]);
+}
+echo $renderer->render_from_template('tool_moodlenet/import_confirmation', $context);
+
 echo $OUTPUT->footer();

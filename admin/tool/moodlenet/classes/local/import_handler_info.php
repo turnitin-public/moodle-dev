ddbd8b88f6d84eedf1b3623153735c528f01a381
Jake Dallimore <jake@moodle.com>
2020-05-14 13:52:23 +0800

    MDL-68698 tool_moodlenet: add strategy support to the handler registry
    Generate import_handler_info objects, by deferrring the matching code
    to the strategy.

--- admin/tool/moodlenet/classes/local/import_handler_info.php
+++ admin/tool/moodlenet/classes/local/import_handler_info.php
@@ -26,15 +26,12 @@ namespace tool_moodlenet\local;
 /**
  * The import_handler_info class.
  *
- * An import_handler_info object represent an extension handler for a particular module.
+ * An import_handler_info object represent an resource import handler for a particular module.
  *
  * @copyright 2020 Jake Dallimore <jrhdallimore@gmail.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class import_handler_info {
-
-    /** @var string $extension the extension. */
-    protected $extension;
 
     /** @var string $modulename the name of the module. */
     protected $modulename;
@@ -42,35 +39,27 @@ class import_handler_info {
     /** @var string $description the description. */
     protected $description;
 
+    /** @var import_strategy $importstrategy the strategy which will be used to import resources handled by this handler */
+    protected $importstrategy;
+
     /**
      * The import_handler_info constructor.
      *
-     * @param string $extension the extension this handler will be responsible for.
      * @param string $modulename the name of the module handling the file extension. E.g. 'label'.
      * @param string $description A description of how the module handles files of this extension type.
+     * @param import_strategy $strategy the strategy which will be used to import the resource.
+     * @throws \coding_exception
      */
-    public function __construct(string $extension, string $modulename, string $description) {
-        if (empty($extension)) {
-            throw new \coding_exception("Extension cannot be empty.");
-        }
+    public function __construct(string $modulename, string $description, import_strategy $strategy) {
         if (empty($modulename)) {
             throw new \coding_exception("Module name cannot be empty.");
         }
         if (empty($description)) {
             throw new \coding_exception("Description cannot be empty.");
         }
-        $this->extension = $extension;
         $this->modulename = $modulename;
         $this->description = $description;
-    }
-
-    /**
-     * Get the extension.
-     *
-     * @return string the extension, e.g. 'txt'.
-     */
-    public function get_extension(): string {
-        return $this->extension;
+        $this->importstrategy = $strategy;
     }
 
     /**
@@ -89,5 +78,14 @@ class import_handler_info {
      */
     public function get_description(): string {
         return $this->description;
+    }
+
+    /**
+     * Get the import strategy used by this handler.
+     *
+     * @return import_strategy the import strategy object.
+     */
+    public function get_strategy(): import_strategy {
+        return $this->importstrategy;
     }
 }

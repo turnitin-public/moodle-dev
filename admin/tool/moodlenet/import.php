44452462fe362e694fd98b7bd8054bdb6e28db56
Jake Dallimore <jake@moodle.com>
2020-05-16 10:40:21 +0800

    MDL-68698 tool_moodlenet: added name and description support
    Present the name during the workflow steps and when saving the mod.
    Description is not shown during the workflow, but it set after creating
    the mod if either of the following is true:
    a) intro wasn't set by the mod during creation (i.e. the field is empty)
    b) the mod is one of an approved list of mods supporting post-setting of
    the description field. Only those mods which do not require description
    as part of their display are able to have their descriptions overridden.

--- admin/tool/moodlenet/import.php
+++ admin/tool/moodlenet/import.php
@@ -35,26 +35,34 @@ if (!get_config('core', 'enablemoodlenet')) {
     print_error('moodlenetnotenabled', 'tool_moodlenet');
 }
 
-$course = optional_param('course', null, PARAM_INT);
-$section = optional_param('section', null, PARAM_INT);
-$type = optional_param('type', 'link', PARAM_INT);
-
 // The POST data must be present and valid.
 if (!empty($_POST)) {
-    if (!empty($_POST['resourceurl'])) {
+    // We require resourceurl as well as resource_info->name.
+    $resourceurl = $_POST['resourceurl'] ?? null;
+    $resourceurl = validate_param($resourceurl, PARAM_URL);
+    $resourceinfo = isset($_POST['resource_info']) ? json_decode($_POST['resource_info']) : null;
+    $course = $_POST['course'] ?? null;
+    $section = $_POST['section'] ?? null;
+    $type = $_POST['type'] ?? 'link';
+    $valid = !is_null($resourceurl) && !is_null($resourceinfo) && !empty($resourceinfo->name);
+    if ($valid) {
         // Take the params we need, create a local URL, and redirect to it.
         // This allows us to hook into the 'wantsurl' capability of the auth system.
-        $resourceurl = validate_param($_POST['resourceurl'], PARAM_URL);
         $resourceurl = urlencode($resourceurl);
+        $name = $resourceinfo->name;
+        $description = $resourceinfo->summary ?? '';
 
         // Build the URL to fetch.
-        $url = new moodle_url('/admin/tool/moodlenet/index.php', ['resourceurl' => $resourceurl, 'type' => $type]);
+        $url = new moodle_url('/admin/tool/moodlenet/index.php', ['resourceurl' => $resourceurl, 'name' => urlencode($name), 'type' => $type]);
 
         if (!is_null($course)) {
             $url->param('course', $course);
         }
         if (!is_null($section)) {
             $url->param('section', $section);
+        }
+        if (!is_null($description)) {
+            $url->param('description', urlencode($description));
         }
 
         redirect($url);

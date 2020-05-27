Chocolate-lightning/moodle

MDL-68561 core: add 'defaultmoodlenet' setting to advanced features
This setting will allow the MoodleNet integration to work if enabled and plugin is removed.

admin/settings/subsystems.php
@@ -77,4 +77,7 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
 
     $optionalsubsystems->add(new admin_setting_configcheckbox('enablemoodlenet', new lang_string('enablemoodlenet', 'admin'),
         new lang_string('enablemoodlenet_desc', 'admin'), 1, 1, 0));
+
+    $optionalsubsystems->add(new admin_setting_configtext('defaultmoodlenet', new lang_string('defaultmoodlenet', 'admin'),
+        new lang_string('defaultmoodlenet_desc', 'admin'), 'https://home.moodle.net'));
 }

turnitin-public/moodle-dev

admin/settings/subsystems.php
@@ -77,4 +77,7 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
 
     $optionalsubsystems->add(new admin_setting_configcheckbox('enablemoodlenet', new lang_string('enablemoodlenet', 'admin'),
         new lang_string('enablemoodlenet_desc', 'admin'), 1, 1, 0));
+
+    $optionalsubsystems->add(new admin_setting_configtext('defaultmoodlenet', new lang_string('defaultmoodlenet', 'admin'),
+        new lang_string('defaultmoodlenet_desc', 'admin'), 'https://team.moodle.net'));
 }
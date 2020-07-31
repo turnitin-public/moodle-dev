7a6bbaf587cad7ec09ce863bde78fdf1ecee2287
Jake Dallimore <jake@moodle.com>
2020-07-31 12:14:00 +0800

    tool mod_tooltest: add dummy module to use as the tool

---
+++ mod/tooltest/lang/en/tooltest.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_tooltest
+ * @category    string
+ * @copyright   2020 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'Tool Test';
+$string['modulename_help'] = 'Tool test is a module which can be used to test the LTI 1.3 provider capabilities in Moodle.';
+$string['modulenameplural'] = 'Tool Tests';
+$string['tooltestfieldset'] = 'Custom example fieldset.';
+$string['tooltestname'] = 'New Tool Test';
+$string['tooltestname_help'] = 'This is the name of the module';
+$string['tooltestsettings'] = 'Settings';
+$string['nonewmodules'] = 'No newmodules'; // TODO check needed.
+$string['pluginadministration'] = 'Tool Test administration';
+$string['pluginname'] = 'Tool Test';
+$string['privacy:metadata'] = 'Tool Test does not store any personal data';
+$string['view'] = 'View';

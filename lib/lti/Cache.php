75f9419788e641fff3b512814d1ba9ea612d7787
Jake Dallimore <jake@moodle.com>
2020-08-01 17:18:46 +0800

    MDL-69543 libraries: Add the LTI 1.3 library
    Part of MDL-69542.

---
+++ lib/lti/Cache.php
@@ -0,0 +1,10 @@
+<?php
+namespace IMSGlobal\LTI13;
+
+interface Cache {
+    public function get_launch_data($key);
+    public function cache_launch_data($key, $jwt_body);
+    public function cache_nonce($nonce);
+    public function check_nonce($nonce);
+}
+?>
---
+++ lib/lti/Database.php
@@ -0,0 +1,9 @@
+<?php
+namespace IMSGlobal\LTI13;
+
+interface Database {
+    public function find_registration_by_issuer($iss, $client_id = null);
+    public function find_deployment($iss, $deployment_id, $client_id = null);
+}
+
+?>

4eb56c14b846e675b485b9b7f8cae352aafb3c49
Jake Dallimore <jake@moodle.com>
2021-07-19 11:50:58 +0800

    MDL-69862 enrol_lti: add dynamic registration support
    Part of MDL-69542.
    This comprises the following changes:
    - Allow the tool to generate secure, one time, dynamic registration
    URLs for use in supporting platforms.
    - Registration endpoint, which validates the one time URL, makes
    the registration requqest to the platform and adds the approriate
    tool registration changes in the tool on success.
    - Some small changes to the admin settings pages, to make use of the
    'copy to clipboard' module - now in core.

---
+++ enrol/lti/tests/local/ltiadvantage/repository/registration_url_repository_test.php
@@ -0,0 +1,137 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace enrol_lti\local\ltiadvantage\repository;
+
+use enrol_lti\local\ltiadvantage\entity\registration_url;
+
+/**
+ * Tests for the registration_url_repository class.
+ *
+ * @package enrol_lti
+ * @copyright 2021 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class registration_url_repository_testcase extends \advanced_testcase {
+    /**
+     * Setup run for each test case.
+     */
+    protected function setUp(): void {
+        $this->resetAfterTest();
+    }
+
+    /**
+     * Test saving a new registration_url instance.
+     */
+    public function test_save_new() {
+        global $DB;
+        $regurl = new registration_url(3600, 'token');
+        $regurlrepo = new registration_url_repository();
+        $savedregurl = $regurlrepo->save($regurl);
+
+        $this->assertEquals($regurl, $savedregurl);
+        $rec = $DB->get_record('enrol_lti_reg_token', []);
+        $this->assertEquals('token', $rec->token);
+        $this->assertEquals(3600, $rec->expirytime);
+    }
+
+    /**
+     * Test saving an existing registration_url instance.
+     */
+    public function test_save_existing() {
+        global $DB;
+        $regurl = new registration_url(time() + 3600, 'token');
+        $regurlrepo = new registration_url_repository();
+        $savedregurl = $regurlrepo->save($regurl);
+        $this->assertEquals(1, $DB->count_records('enrol_lti_reg_token'));
+
+        $regurlrepo->save($savedregurl);
+        $this->assertEquals(1, $DB->count_records('enrol_lti_reg_token'));
+    }
+
+    /**
+     * Test finding the registration_url.
+     * @dataProvider find_data_provider
+     * @param int $expirytime the expiry time of the registration URL.
+     * @param bool $found whether the find() method is expected to return the URL or not.
+     */
+    public function test_find(int $expirytime, bool $found) {
+        $regurl = new registration_url($expirytime, 'token');
+        $regurlrepo = new registration_url_repository();
+
+        // No URL saved yet, so nothing found.
+        $this->assertNull($regurlrepo->find());
+
+        // Now save and confirm the find returns the correct URL, or null.
+        $regurlrepo->save($regurl);
+
+        if ($found) {
+            $this->assertEquals($regurl, $regurlrepo->find());
+        } else {
+            $this->assertNull($regurlrepo->find());
+        }
+    }
+
+    /**
+     * Data provider for testing the find() method.
+     *
+     * @return array the test data.
+     */
+    public function find_data_provider() {
+        return [
+            'still valid' => [
+                'expirytime' => time() + 3600,
+                'found' => true
+            ],
+            'expired' => [
+                'expirytime' => 1000,
+                'found' => false
+            ]
+        ];
+    }
+
+    /**
+     * Test finding a registration URL by its token.
+     */
+    public function test_find_by_token() {
+        $regurl = new registration_url(time() + 3600, 'token');
+        $regurlrepo = new registration_url_repository();
+
+        // No URL saved yet, so nothing found.
+        $this->assertNull($regurlrepo->find_by_token('token'));
+
+        // Now save and confirm the find returns the correct URL, or null.
+        $regurlrepo->save($regurl);
+
+        $this->assertEquals($regurl, $regurlrepo->find_by_token('token'));
+        $this->assertNull($regurlrepo->find_by_token('notfound'));
+    }
+
+    /**
+     * Test deleting the registration_url.
+     */
+    public function test_delete() {
+        global $DB;
+        $regurl = new registration_url(time() + 3600, 'token');
+        $regurlrepo = new registration_url_repository();
+        $regurlrepo->save($regurl);
+        $this->assertInstanceOf(registration_url::class, $regurlrepo->find());
+
+        $regurlrepo->delete();
+        $this->assertNull($regurlrepo->find());
+        $this->assertFalse($DB->record_exists('enrol_lti_reg_token', []));
+    }
+}

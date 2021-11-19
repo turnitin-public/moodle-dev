aca2bee4a3c572430869a0d9e9d4868d70288844
Jake Dallimore <jake@moodle.com>
2021-11-19 17:29:59 +0800

    fixup! MDL-69543 enrol_lti: add admin page for deployment management

--- enrol/lti/classes/local/ltiadvantage/form/deployment_form.php
+++ enrol/lti/classes/local/ltiadvantage/form/deployment_form.php
@@ -14,14 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Contains the deployment form class.
- *
- * @package    enrol_lti
- * @copyright  2021 Jake Dallimore <jrhdallimore@gmail.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 namespace enrol_lti\local\ltiadvantage\form;
 use enrol_lti\local\ltiadvantage\repository\deployment_repository;
 

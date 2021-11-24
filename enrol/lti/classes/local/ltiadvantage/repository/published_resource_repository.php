269297f0c2ce74f2ac1c58262a886b4b58417041
Jake Dallimore <jake@moodle.com>
2021-11-24 12:00:09 +0800

    MDL-72745 enrol_lti: fix bug affecting access to published resources
    Now check the course role so we're sure the user can view the list of
    published resources.

--- enrol/lti/classes/local/ltiadvantage/repository/published_resource_repository.php
+++ enrol/lti/classes/local/ltiadvantage/repository/published_resource_repository.php
@@ -151,6 +151,11 @@ class published_resource_repository {
         $params = array_merge($inparams, ['enrolstatusenabled' => ENROL_INSTANCE_ENABLED]);
         $resources = $DB->get_records_sql($sql, $params);
 
+        // Only users who have the ability to publish content should see published content.
+        $resources = array_filter($resources, function($resource) use ($userid) {
+            return has_capability('enrol/lti:config', \context_course::instance($resource->courseid), $userid);
+        });
+
         // Make sure the user can access each course or module, excluding those which are inaccessible from the return.
         $availableresources = $this->get_available_resources_from_records($resources, $userid);
 

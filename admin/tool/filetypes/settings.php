3f3f98eb6bb7c7c71aac157796fa755ac1af1560
Jake Dallimore <jake@moodle.com>
2022-09-12 13:15:39 +0800

    MDL-75650 tool_filetypes: make sure admin settings link is second last
    This just ensures the 'File types' link in 'Server' remains second to
    last, after tool_oauth2 was moved to core_oauth2 and will now be added
    before filetypes.

--- admin/tool/filetypes/settings.php
+++ admin/tool/filetypes/settings.php
@@ -25,7 +25,11 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) {
-    $ADMIN->add('server', new admin_externalpage('tool_filetypes',
+    $ADMIN->add(
+        'server',
+        new admin_externalpage('tool_filetypes',
             new lang_string('pluginname', 'tool_filetypes'),
-            $CFG->wwwroot . '/admin/tool/filetypes/index.php'));
+            $CFG->wwwroot . '/admin/tool/filetypes/index.php'),
+        'oauth2'
+    );
 }

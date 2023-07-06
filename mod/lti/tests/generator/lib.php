4377e4dd90cc0239224eae962510814040bd0c3e
Jake Dallimore <jake@moodle.com>
2023-07-06 15:15:59 +0800

    MDL-78575 mod_lti: add behat coverage of course tools list page

--- mod/lti/tests/generator/lib.php
+++ mod/lti/tests/generator/lib.php
@@ -100,4 +100,12 @@ class mod_lti_generator extends testing_module_generator {
         }
         lti_add_type((object) $type, (object) $config);
     }
+
+    public function create_course_tool_types(array $type, ?array $config = null) {
+        if (!isset($type['baseurl'])) {
+            throw new coding_exception('Must specify baseurl when creating a LTI tool type.');
+        }
+        lti_add_type((object) $type, (object) $config);
+
+    }
 }

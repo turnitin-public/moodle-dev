0b2ee71480e1539d3649545145ea96c063442ca5
Alex Morris <alex.morris@catalyst.net.nz>
2023-10-17 12:16:55 +1300

    MDL-79113 ltix: Migrate types and tool functions from mod_lti
    Co-authored-by: godsonahamba <133884815+godsonahamba@users.noreply.github.com>

--- lib/db/access.php
+++ lib/db/access.php
@@ -2741,4 +2741,14 @@ $capabilities = array(
             'manager' => CAP_ALLOW,
         ]
     ],
+
+    // The ability to create or edit tool configurations for particular courses.
+    'moodle/ltix:addcoursetool' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
 );

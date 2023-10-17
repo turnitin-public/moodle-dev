8fa0b354bc684e9346b9e954db576ffb932d938a
Alex Morris <alex.morris@catalyst.net.nz>
2023-10-18 11:56:27 +1300

    MDL-79113 ltix: Move constants to new core file

--- mod/lti/typessettings.php
+++ mod/lti/typessettings.php
@@ -52,6 +52,7 @@ require_once('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->dirroot.'/mod/lti/edit_form.php');
 require_once($CFG->dirroot.'/mod/lti/locallib.php');
+require_once($CFG->dirroot.'/ltix/constants.php');
 
 $action       = optional_param('action', null, PARAM_ALPHANUMEXT);
 $id           = optional_param('id', null, PARAM_INT);

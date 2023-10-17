c2d102f7e1693d31763388f91cbd4d2d94ee4b14
Alex Morris <alex.morris@catalyst.net.nz>
2023-10-18 11:56:27 +1300

    MDL-79113 ltix: Move constants to new core file

--- ltix/classes/types_helper.php
+++ ltix/classes/types_helper.php
@@ -18,52 +18,15 @@ declare(strict_types=1);
 
 namespace core_ltix;
 
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/ltix/constants.php');
+
 use context_course;
 use core\context\course;
 use mod_lti\local\ltiopenid\registration_helper;
 use moodle_exception;
 use stdClass;
-
-define('LTI_URL_DOMAIN_REGEX', '/(?:https?:\/\/)?(?:www\.)?([^\/]+)(?:\/|$)/i');
-
-define('LTI_LAUNCH_CONTAINER_DEFAULT', 1);
-define('LTI_LAUNCH_CONTAINER_EMBED', 2);
-define('LTI_LAUNCH_CONTAINER_EMBED_NO_BLOCKS', 3);
-define('LTI_LAUNCH_CONTAINER_WINDOW', 4);
-define('LTI_LAUNCH_CONTAINER_REPLACE_MOODLE_WINDOW', 5);
-
-define('LTI_TOOL_STATE_ANY', 0);
-define('LTI_TOOL_STATE_CONFIGURED', 1);
-define('LTI_TOOL_STATE_PENDING', 2);
-define('LTI_TOOL_STATE_REJECTED', 3);
-define('LTI_TOOL_PROXY_TAB', 4);
-
-define('LTI_TOOL_PROXY_STATE_CONFIGURED', 1);
-define('LTI_TOOL_PROXY_STATE_PENDING', 2);
-define('LTI_TOOL_PROXY_STATE_ACCEPTED', 3);
-define('LTI_TOOL_PROXY_STATE_REJECTED', 4);
-
-define('LTI_SETTING_NEVER', 0);
-define('LTI_SETTING_ALWAYS', 1);
-define('LTI_SETTING_DELEGATE', 2);
-
-define('LTI_COURSEVISIBLE_NO', 0);
-define('LTI_COURSEVISIBLE_PRECONFIGURED', 1);
-define('LTI_COURSEVISIBLE_ACTIVITYCHOOSER', 2);
-
-define('LTI_VERSION_1', 'LTI-1p0');
-define('LTI_VERSION_2', 'LTI-2p0');
-define('LTI_VERSION_1P3', '1.3.0');
-define('LTI_RSA_KEY', 'RSA_KEY');
-define('LTI_JWK_KEYSET', 'JWK_KEYSET');
-
-define('LTI_DEFAULT_ORGID_SITEID', 'SITEID');
-define('LTI_DEFAULT_ORGID_SITEHOST', 'SITEHOST');
-
-define('LTI_ACCESS_TOKEN_LIFE', 3600);
-
-// Standard prefix for JWT claims.
-define('LTI_JWT_CLAIM_PREFIX', 'https://purl.imsglobal.org/spec/lti');
 
 /**
  * Helper class specifically dealing with LTI types (preconfigured tools).
--- mod/lti/locallib.php
+++ mod/lti/locallib.php
@@ -59,47 +59,7 @@ require_once($CFG->dirroot.'/ltix/OAuth.php');
 require_once($CFG->libdir.'/weblib.php');
 require_once($CFG->dirroot . '/course/modlib.php');
 require_once($CFG->dirroot . '/ltix/TrivialStore.php');
-
-define('LTI_URL_DOMAIN_REGEX', '/(?:https?:\/\/)?(?:www\.)?([^\/]+)(?:\/|$)/i');
-
-define('LTI_LAUNCH_CONTAINER_DEFAULT', 1);
-define('LTI_LAUNCH_CONTAINER_EMBED', 2);
-define('LTI_LAUNCH_CONTAINER_EMBED_NO_BLOCKS', 3);
-define('LTI_LAUNCH_CONTAINER_WINDOW', 4);
-define('LTI_LAUNCH_CONTAINER_REPLACE_MOODLE_WINDOW', 5);
-
-define('LTI_TOOL_STATE_ANY', 0);
-define('LTI_TOOL_STATE_CONFIGURED', 1);
-define('LTI_TOOL_STATE_PENDING', 2);
-define('LTI_TOOL_STATE_REJECTED', 3);
-define('LTI_TOOL_PROXY_TAB', 4);
-
-define('LTI_TOOL_PROXY_STATE_CONFIGURED', 1);
-define('LTI_TOOL_PROXY_STATE_PENDING', 2);
-define('LTI_TOOL_PROXY_STATE_ACCEPTED', 3);
-define('LTI_TOOL_PROXY_STATE_REJECTED', 4);
-
-define('LTI_SETTING_NEVER', 0);
-define('LTI_SETTING_ALWAYS', 1);
-define('LTI_SETTING_DELEGATE', 2);
-
-define('LTI_COURSEVISIBLE_NO', 0);
-define('LTI_COURSEVISIBLE_PRECONFIGURED', 1);
-define('LTI_COURSEVISIBLE_ACTIVITYCHOOSER', 2);
-
-define('LTI_VERSION_1', 'LTI-1p0');
-define('LTI_VERSION_2', 'LTI-2p0');
-define('LTI_VERSION_1P3', '1.3.0');
-define('LTI_RSA_KEY', 'RSA_KEY');
-define('LTI_JWK_KEYSET', 'JWK_KEYSET');
-
-define('LTI_DEFAULT_ORGID_SITEID', 'SITEID');
-define('LTI_DEFAULT_ORGID_SITEHOST', 'SITEHOST');
-
-define('LTI_ACCESS_TOKEN_LIFE', 3600);
-
-// Standard prefix for JWT claims.
-define('LTI_JWT_CLAIM_PREFIX', 'https://purl.imsglobal.org/spec/lti');
+require_once($CFG->dirroot . '/ltix/constants.php');
 
 /**
  * Return the mapping for standard message types to JWT message_type claim.

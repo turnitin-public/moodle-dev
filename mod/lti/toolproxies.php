23350d0a322d8e969b591e78072cd9c2583802e0
Alex Morris <alex.morris@catalyst.net.nz>
2023-10-18 11:56:27 +1300

    MDL-79113 ltix: Move constants to new core file

--- mod/lti/toolproxies.php
+++ mod/lti/toolproxies.php
@@ -26,6 +26,7 @@
 require_once('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->dirroot.'/mod/lti/locallib.php');
+require_once($CFG->dirroot.'/ltix/constants.php');
 
 // No guest autologin.
 require_login(0, false);
